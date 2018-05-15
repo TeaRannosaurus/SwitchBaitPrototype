9be993697745874a7e14a9dfd33b3f37d78e7f6f
Lars <Lars.Kruik@live.nl>
2018-05-15 13:22:22 +0200

    Added entityspawners

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -2,15 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemySpawner : MonoBehaviour {
+public class EnemySpawner : MonoBehaviour
+{
+    public List<GameObject> objectsToSpawn = null;
+    public Vector2 spawnDelayMinMax = new Vector2(0.5f, 2.0f);
+    public int maxAmountOfEntitys = 10;
+    public bool isInfinite = false;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private int entityCounter;
+
+    private void Start()
+    {
+        entityCounter = maxAmountOfEntitys;
+        StartCoroutine("SpawnEntity");
+    }
+
+    private IEnumerator SpawnEntity()
+    {
+        while ((maxAmountOfEntitys < entityCounter) || isInfinite)
+        {
+            GameObject newEntity = GameObject.Instantiate(objectsToSpawn[Random.Range(0, objectsToSpawn.Count)], transform.position, Quaternion.identity);
+
+            entityCounter--;
+            yield return new WaitForSeconds(Random.Range(spawnDelayMinMax.x, spawnDelayMinMax.y));
+        }
+
+    }
+
+    private void Update()
+    {
+        
+    }
+
 }
